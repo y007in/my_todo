f8e93822f9cd1de5ea79cb8a16cbd164289ff0b5
yoojin <yoojin4719@gmail.com>
2023-07-14 17:42:00 +0900

    localstorage

--- src/App.js
+++ src/App.js
@@ -1,34 +1,63 @@
 import "./App.css";
-import { useRef, useReducer } from "react";
+import { useRef, useReducer, useEffect, useState } from "react";
 import Header from "./Components/Header";
 import TodoEditor from "./Components/TodoEditor";
 import TodoList from "./Components/TodoList";
 function reducer(state, action) {
   switch (action.type) {
     case "CREATE":
-      return [action.newItem, ...state];
+      const newState = [action.newItem, ...state];
+      localStorage.setItem(`todo`, JSON.stringify(newState));
+      return newState;
     case "UPDATE": {
       return state.map((item) =>
         item.id === action.targetId ? { ...item, isDone: !item.isDone } : item
       );
     }
     case "DELETE": {
-      return state.filter((item) => item.id !== action.targetId);
+      const newState = state.filter((item) => item.id !== action.targetId);
+      localStorage.setItem(`todo`, JSON.stringify(newState));
+      return newState;
     }
     case "EDIT": {
-      return state.map((item) =>
+      const newState = state.map((item) =>
         item.id === action.targetId
           ? { ...item, content: action.newContent }
           : item
       );
+      return newState;
+    }
+    case "INIT": {
+      return action.data;
     }
     default:
       return state;
   }
 }
 function App() {
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
   const [todo, dispatch] = useReducer(reducer, []);
   const idRef = useRef(0);
+  useEffect(() => {
+    const rawData = localStorage.getItem("todo");
+    if (!rawData) {
+      setIsDataLoaded(true);
+      return;
+    }
+    const localData = JSON.parse(rawData);
+    if (localData.length === 0) {
+      setIsDataLoaded(true);
+      return;
+    }
+
+    idRef.current = localData[0].id + 1;
+    dispatch({ type: "INIT", data: localData });
+    setIsDataLoaded(true);
+    return;
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("todo", JSON.stringify(todo));
+  }, [todo]);
   const onCreate = (content) => {
     dispatch({
       type: "CREATE",
@@ -60,20 +89,24 @@ function App() {
       newContent,
     });
   };
-  return (
-    <div className="App">
-      <div className="container">
-        <Header todo={todo} />
-        <TodoEditor onCreate={onCreate} />
-        <TodoList
-          todo={todo}
-          onUpdate={onUpdate}
-          onDelete={onDelete}
-          onEdit={onEdit}
-        />
+  if (!isDataLoaded) {
+    return <div>데이터를 불러오는 중입니다.</div>;
+  } else {
+    return (
+      <div className="App">
+        <div className="container">
+          <Header todo={todo} />
+          <TodoEditor onCreate={onCreate} />
+          <TodoList
+            todo={todo}
+            onUpdate={onUpdate}
+            onDelete={onDelete}
+            onEdit={onEdit}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
